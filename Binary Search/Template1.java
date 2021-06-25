ad778d15d7bf3a7f53bb7a1bff9b5f5cbc85169d
Mika <77134715+Mikahu512@users.noreply.github.com>
2021-06-25 15:32:24 -0500

    Update Template1.java

--- Binary Search/Template1.java
+++ Binary Search/Template1.java
@@ -6,7 +6,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
+ 
 public class WriteToFileExample {
   public static void main(String[] args) {
 		try {

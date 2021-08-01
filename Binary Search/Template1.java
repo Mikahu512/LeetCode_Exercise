19951f22495453c8ab2360d96b839b32d175e4b1
Mika <77134715+Mikahu512@users.noreply.github.com>
2021-08-01 16:27:49 -0500

    Update Template1.java

--- Binary Search/Template1.java
+++ Binary Search/Template1.java
@@ -15,7 +15,7 @@ public class WriteToFileExample {
 			// if file doesnt exists, then create it
 			if (!file.exists()) { 
 				file.createNewFile();
-			}  
+			} 
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.write(content);
